aedaa510807fd0d4dc93f94cd98a078231ae7bc6
layetri <daniel@mainstreamct.com>
2024-12-08 14:45:22 +0100

    Replace Vec<_> with slice where possible for compatibility with SmallVec

--- rsworld/src/lib.rs
+++ rsworld/src/lib.rs
@@ -19,7 +19,7 @@ use rsworld_sys::{
     Synthesis,
 };
 
-pub fn cheaptrick(x: &Vec<f64>, fs: i32, temporal_positions: &Vec<f64>, f0: &Vec<f64>, option: &mut CheapTrickOption) -> Vec<Vec<f64>> {
+pub fn cheaptrick(x: &[f64], fs: i32, temporal_positions: &[f64], f0: &[f64], option: &mut CheapTrickOption) -> Vec<Vec<f64>> {
     let x_length:  i32 = x.len()  as i32;
     let f0_length: i32 = f0.len() as i32;
     unsafe {
@@ -40,7 +40,7 @@ pub fn get_number_of_aperiodicities(fs: i32) -> i32 {
     }
 }
 
-pub fn code_aperiodicity(aperiodicity: &Vec<Vec<f64>>, f0_length: i32, fs: i32) -> Vec<Vec<f64>> {
+pub fn code_aperiodicity(aperiodicity: &[Vec<f64>], f0_length: i32, fs: i32) -> Vec<Vec<f64>> {
     let mut cheaptrick_option = CheapTrickOption::new(fs);
     unsafe {
 	GetFFTSizeForCheapTrick(fs, &mut cheaptrick_option as *mut _);
@@ -61,7 +61,7 @@ pub fn code_aperiodicity(aperiodicity: &Vec<Vec<f64>>, f0_length: i32, fs: i32) 
     coded_aperiodicity
 }
 
-pub fn decode_aperiodicity(coded_aperiodicity: &Vec<Vec<f64>>, f0_length: i32, fs: i32) -> Vec<Vec<f64>> {
+pub fn decode_aperiodicity(coded_aperiodicity: &[Vec<f64>], f0_length: i32, fs: i32) -> Vec<Vec<f64>> {
     let mut cheaptrick_option = CheapTrickOption::new(fs);
     unsafe {
 	GetFFTSizeForCheapTrick(fs, &mut cheaptrick_option as *mut _);
@@ -78,7 +78,7 @@ pub fn decode_aperiodicity(coded_aperiodicity: &Vec<Vec<f64>>, f0_length: i32, f
     aperiodicity
 }
 
-pub fn code_spectral_envelope(spectrogram: &Vec<Vec<f64>>, f0_length: i32, fs: i32, fft_size: i32, number_of_dimensions: i32) -> Vec<Vec<f64>> {
+pub fn code_spectral_envelope(spectrogram: &[Vec<f64>], f0_length: i32, fs: i32, fft_size: i32, number_of_dimensions: i32) -> Vec<Vec<f64>> {
     let spectrogram_ptr = spectrogram.iter().map(|inner| inner.as_ptr()).collect::<Vec<_>>();
     let spectrogram_ptr = spectrogram_ptr.as_ptr();
     let mut coded_spectral_envelope     = vec![vec![0.0; number_of_dimensions as usize]; f0_length as usize];
@@ -91,7 +91,7 @@ pub fn code_spectral_envelope(spectrogram: &Vec<Vec<f64>>, f0_length: i32, fs: i
     coded_spectral_envelope
 }
 
-pub fn decode_spectral_envelope(coded_spectrogram: &Vec<Vec<f64>>, f0_length: i32, fs: i32, fft_size: i32) -> Vec<Vec<f64>> {
+pub fn decode_spectral_envelope(coded_spectrogram: &[Vec<f64>], f0_length: i32, fs: i32, fft_size: i32) -> Vec<Vec<f64>> {
     let number_of_dimensions = coded_spectrogram[0].len() as i32;
     let mut spectrogram      = vec![vec![0.0; (fft_size/2+1) as usize]; f0_length as usize];
     let mut spectrogram_ptr = spectrogram.iter_mut().map(|inner| inner.as_mut_ptr()).collect::<Vec<_>>();
@@ -104,7 +104,7 @@ pub fn decode_spectral_envelope(coded_spectrogram: &Vec<Vec<f64>>, f0_length: i3
     spectrogram
 }
 
-pub fn d4c(x: &Vec<f64>, fs: i32, temporal_positions: &Vec<f64>, f0: &Vec<f64>, option: &D4COption) -> Vec<Vec<f64>> {
+pub fn d4c(x: &[f64], fs: i32, temporal_positions: &[f64], f0: &[f64], option: &D4COption) -> Vec<Vec<f64>> {
     let x_length  = x.len()  as i32;
     let f0_length = f0.len() as i32;
     let mut cheaptrick_option = CheapTrickOption::new(fs);
@@ -121,7 +121,7 @@ pub fn d4c(x: &Vec<f64>, fs: i32, temporal_positions: &Vec<f64>, f0: &Vec<f64>, 
     aperiodicity
 }
 
-pub fn dio(x: &Vec<f64>, fs: i32, option: &DioOption) -> (Vec<f64>, Vec<f64>) {
+pub fn dio(x: &[f64], fs: i32, option: &DioOption) -> (Vec<f64>, Vec<f64>) {
     let x_length = x.len() as i32;
     let f0_length: usize;
     unsafe {
@@ -135,7 +135,7 @@ pub fn dio(x: &Vec<f64>, fs: i32, option: &DioOption) -> (Vec<f64>, Vec<f64>) {
     (temporal_positions, f0)
 }
 
-pub fn harvest(x: &Vec<f64>, fs: i32, option: &HarvestOption) -> (Vec<f64>, Vec<f64>) {
+pub fn harvest(x: &[f64], fs: i32, option: &HarvestOption) -> (Vec<f64>, Vec<f64>) {
     let x_length = x.len() as i32;
     let f0_length: usize;
     unsafe {
@@ -149,7 +149,7 @@ pub fn harvest(x: &Vec<f64>, fs: i32, option: &HarvestOption) -> (Vec<f64>, Vec<
     (temporal_positions, f0)
 }
 
-pub fn stonemask(x: &Vec<f64>, fs: i32, temporal_positions: &Vec<f64>, f0: &Vec<f64>) -> Vec<f64> {
+pub fn stonemask(x: &[f64], fs: i32, temporal_positions: &[f64], f0: &[f64]) -> Vec<f64> {
     let x_length  = x.len() as i32;
     let f0_length = f0.len();
     let mut refined_f0 = vec![0.0; f0_length];
@@ -159,7 +159,7 @@ pub fn stonemask(x: &Vec<f64>, fs: i32, temporal_positions: &Vec<f64>, f0: &Vec<
     refined_f0
 }
 
-pub fn synthesis(f0: &Vec<f64>, spectrogram: &Vec<Vec<f64>>, aperiodicity: &Vec<Vec<f64>>, frame_period: f64, fs: i32) -> Vec<f64> {
+pub fn synthesis(f0: &[f64], spectrogram: &[Vec<f64>], aperiodicity: &[Vec<f64>], frame_period: f64, fs: i32) -> Vec<f64> {
     let f0_length        = f0.len() as i32;
     let spectrogram_ptr  = spectrogram.iter().map(|inner| inner.as_ptr()).collect::<Vec<_>>();
     let spectrogram_ptr  = spectrogram_ptr.as_ptr();
